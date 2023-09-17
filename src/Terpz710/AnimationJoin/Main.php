68031532bcd4c5db43f0db75328d818a25e35787
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-16 23:33:23 -0700

    Update Main.php

--- src/Terpz710/AnimationJoin/Main.php
+++ src/Terpz710/AnimationJoin/Main.php
@@ -3,9 +3,8 @@
 namespace Terpz710\JoinAnimations;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase {
 
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
