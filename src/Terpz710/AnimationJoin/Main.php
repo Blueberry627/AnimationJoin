9b288f515ced2a70007d4b74d19645dc1a0154bd
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-09-16 22:35:31 -0700

    Update Main.php

--- src/Terpz710/AnimationJoin/Main.php
+++ src/Terpz710/AnimationJoin/Main.php
@@ -3,7 +3,7 @@
 namespace Terpz710\AnimationJoin;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
 use pocketmine\network\mcpe\protocol\ActorEventPacket;
@@ -17,27 +17,20 @@ class Main extends PluginBase implements Listener {
     private $config;
 
     public function onEnable(): void {
-        
         $this->saveDefaultConfig();
-
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onInteract(PlayerInteractEvent $event) {
+    public function onJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
-        $action = $event->getItem();
         $animationType = $this->config->get("animation_type", "totem");
 
-        if ($action instanceof Item && $action->getId() === Item::TOTEM) {
-            if ($animationType === "totem") {
-                
-                $this->sendCustomAnimation($player, ActorEvent::CONSUME_TOTEM);
-            } elseif ($animationType === "guardian") {
-
-                $player->addEffect(Effect::getEffect(Effect::GUARDIAN_CURSE)->setDuration(200));
-            }
+        if ($animationType === "totem") {
+            $this->sendCustomAnimation($player, ActorEvent::CONSUME_TOTEM);
+        } elseif ($animationType === "guardian") {
+            $player->addEffect(Effect::getEffect(Effect::GUARDIAN_CURSE)->setDuration(200));
         }
     }
 
